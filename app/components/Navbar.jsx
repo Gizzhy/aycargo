36d121135e232648417d44018285618fa5522cf9
Gizzhy <gizzhyakinz@gmail.com>
2025-05-24 21:38:41 +0100

    navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./styles/nav.module.scss";
+import logo from "../../assets/images/logo.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className={styles.navbar}>
+        <Image src={logo} alt="aycargo logo" />
+        <div>
+          <a href="/#">Services</a>
+          <a href="/#">Locations</a>
+          <a href="/#">Our Strength</a>
+          <a href="/#">About us</a>
+        </div>
+        <div>
+          <button>Request a quote</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
